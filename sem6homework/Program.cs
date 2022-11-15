0ce785cad9715df80e10942efe46b4f2489c60b1
Ivan <ab09@inbox.ru>
2022-11-15 22:53:58 +0500

    Task 41

---
+++ sem6homework/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+Console.Clear();
+
+/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 4
+*/
+
+// Создаем функцию, которая принимает с консоли числа в формате строки и создает целочисленный массив
+
+int[] GetNumbers(string numbers)
+{
+    char[] separatores = new char[] { ' ', '.', ',', ';' };
+    string[] arrayNumbers = numbers.Split(separatores, StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int [arrayNumbers.Length];
+    for (int i = 0; i < arrayNumbers.Length; i++)
+    {
+        result[i] = Convert.ToInt32(arrayNumbers[i]);
+    }
+    return result;
+}
+
+int Count(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) result++;
+    }
+    return result;
+}
+
+
+
+
+Console.WriteLine("Введите числа: ");
+string figures = Convert.ToString(Console.ReadLine());
+GetNumbers(figures);
+Console.WriteLine($"[{String.Join(" ", GetNumbers(figures))}]");
+Console.WriteLine("---------------");
+Console.WriteLine($"Среди введенных чисел, чисел больше ноля {Count(GetNumbers(figures))} числа");
